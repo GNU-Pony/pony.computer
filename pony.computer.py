86bdd8dfa8147bbfe40f4a93f959890bbc49876f
Mattias Andrée <maandree@operamail.com>
2014-04-07 02:23:53 +0200

    python port + add wayland display
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- pony.computer.py
+++ pony.computer.py
@@ -63,7 +63,7 @@ valuec = '01;34'
 '''
 
 
-def env(variable, default = None):
+def env(variable, default = ''):
     '''
     Read an environment variable
     
@@ -79,7 +79,7 @@ def spawn(*command):
     
     @param  command:*str  The command arguments
     '''
-    Popen(command, sys.stdin = sys.stdin, stdout = sys.stdout, stderr = sys.stderr).wait()
+    Popen(command, stdin = sys.stdin, stdout = sys.stdout, stderr = sys.stderr).wait()
 
 def spawn_pipe(*command):
     '''
@@ -88,10 +88,11 @@ def spawn_pipe(*command):
     @param   command:*str  The command arguments
     @return  :str          The output to the command's stdout, with at most one trailing LF removed
     '''
-    out = Popen(command, sys.stdin = sys.stdin, stdout = PIPE, stderr = sys.stderr).communuicate()[0]
+    out = Popen(command, stdin = sys.stdin, stdout = PIPE, stderr = sys.stderr).communicate()[0]
+    out = out.decode('utf-8', 'replace')
     return out[:-1] if out.endswith('\n') else out
 
-def print(text = '', end = '\n', flush = False):
+def print(text = '', end = '\n', flush = True):
     '''
     Hack to enforce UTF-8 in output (in the future, if you see anypony not using utf-8 in
     programs by default, report them to Princess Celestia so she can banish them to the moon)
@@ -104,7 +105,7 @@ def print(text = '', end = '\n', flush = False):
     if flush:
         sys.stdout.buffer.flush()
 
-def printerr(text = '', end = '\n', flush = False):
+def printerr(text = '', end = '\n', flush = True):
     '''
     stderr equivalent to print()
     
@@ -116,54 +117,165 @@ def printerr(text = '', end = '\n', flush = False):
     if flush:
         sys.stderr.buffer.flush()
 
+def printtag(name, value, flush = False):
+    '''
+    Print a tag
+    
+    @param  name:str    The name of the tag
+    @param  value:str   The value of the tag
+    @param  flush:bool  Whether to flush the output
+    '''
+    print('\033[%sm%s:\033[00;%sm %s' % (tagc, name, valuec, value), end = '\n', flush = flush)
 
-def infofunc() ## FIXME not ported
-    echo -e "\e[${left}C\e[${tagc}mUser:\e[00;${valuec}m ${USER}"
-    echo -e "\e[${left}C\e[${tagc}mHome:\e[00;${valuec}m ${HOME}"
-    echo -e "\e[${left}C\e[${tagc}mHostname:\e[00;${valuec}m ${HOSTNAME}"
-    echo -e "\e[${left}C\e[${tagc}mDistribution:\e[00;${valuec}m ${PRETTY_NAME}"
-    echo -e "\e[${left}C\e[${tagc}mOperating system:\e[00;${valuec}m $(uname --operating-system) with $(uname --kernel-name) $(uname --kernel-release) kernel"
-    echo -e "\e[${left}C\e[${tagc}mKernel version:\e[00;${valuec}m $(uname --kernel-version)"
-    echo -e "\e[${left}C\e[${tagc}mProcessor architecture:\e[00;${valuec}m $(uname --machine)"
-    if [ -e /proc/cpuinfo ] && [ ! -d /proc/cpuinfo ]; then
-	cat /proc/cpuinfo | grep '^model name' | sort | uniq | cut -d : -f 2 | while read processor; do
-	    echo -e "\e[${left}C\e[${tagc}mProcessor model:\e[00;${valuec}m ${processor}"
-	done
-	echo -e "\e[${left}C\e[${tagc}mCurrent CPU speed:\e[00;${valuec}m $(python <<< "print($(echo $(cat /proc/cpuinfo | grep '^cpu MHz' | cut -d : -f 2 | sed -e 's:$: +:g') 0))") MHz"
-    fi
-    if [ -e /proc/loadavg ] && [ ! -d /proc/loadavg ]; then
-	echo -e "\e[${left}C\e[${tagc}mLoad average:\e[00;${valuec}m $(cat /proc/loadavg)"
-    fi
-    if [ -e /proc/uptime ] && [ ! -d /proc/uptime ]; then
-	echo -e "\e[${left}C\e[${tagc}mUptime:\e[00;${valuec}m $(cat /proc/uptime)"
-    fi
-    if [ -e /proc/meminfo ] && [ ! -d /proc/meminfo ]; then
-	function mem
-	{
-	    cat /proc/meminfo | grep "^$1:" | cut -d : -f 2
-	}
-	echo -e "\e[${left}C\e[${tagc}mTotal memory:\e[00;${valuec}m" $(mem MemTotal) + $(mem SwapTotal) 'swap'
-	echo -e "\e[${left}C\e[${tagc}mHardware corrupted memory:\e[00;${valuec}m" $(mem HardwareCorrupted)
-	echo -e "\e[${left}C\e[${tagc}mKernel memory:\e[00;${valuec}m" $(mem KernelStack) 'stack +' $(mem Slab) 'slab'
-	echo -e "\e[${left}C\e[${tagc}mShared memory:\e[00;${valuec}m" $(mem Shmem)
-	echo -e "\e[${left}C\e[${tagc}mLocked memory:\e[00;${valuec}m" $(mem Mlocked), $(mem Unevictable) 'unevictable'
-	echo -e "\e[${left}C\e[${tagc}mMemory buffers:\e[00;${valuec}m" $(mem Buffers)
-	echo -e "\e[${left}C\e[${tagc}mCached memory:\e[00;${valuec}m" $(mem Cached) + $(mem SwapCached) 'swap'
-    fi
-    if [ -e /proc/net/route ] && [ ! -d /proc/net/route ]; then
-	python -c 'g=input(); print("%i.%i.%i.%i" % (int(g[6:8], 16), int(g[4:6], 16), int(g[2:4], 16), int(g[0:2], 16)))' <<< "$(cat /proc/net/route | cut -f 3 | grep -v 'Gateway' | grep -v 00000000)" |
-	while read line; do
-	    echo -e "\e[${left}C\e[${tagc}mDefault gateway:\e[00;${valuec}m ${line}"
-	done
-    fi
-    echo -e "\e[${left}C\e[${tagc}mShell:\e[00;${valuec}m ${SHELL}"
-    echo -e "\e[${left}C\e[${tagc}mShell version:\e[00;${valuec}m $("${SHELL}" --version | head -n 1)"
-    echo -e "\e[${left}C\e[${tagc}mTeletypewriter:\e[00;${valuec}m $(tty)"
-    echo -e "\e[${left}C\e[${tagc}mTerminal:\e[00;${valuec}m ${TERM} ${COLORTERM}"
-    echo -e "\e[${left}C\e[${tagc}mX display:\e[00;${valuec}m ${DISPLAY}"
-    echo -e "\e[${left}C\e[${tagc}mWindow manager:\e[00;${valuec}m ${DESKTOP_SESSION}"
-    echo -e "\e[${left}C\e[${tagc}mEditor:\e[00;${valuec}m ${EDITOR}"
-    echo -e "\e[${left}C\e[${tagc}mLocale:\e[00;${valuec}m ${LANG}"
+def try_(*functions):
+    '''
+    Try a series of functions an return the return of whatever function that did not raise an exception
+    
+    @param   functions:*()→¿V?  Functions to try to run
+    @return  :¿V?|''            The return of the first function that works, empty string on total failure
+    '''
+    for function in functions:
+        try:
+            return function()
+        except:
+            pass
+    return ''
+
+def cat(pathname):
+    '''
+    Read a file
+    
+    @param   pathname:str  The file to read
+    @return  :str          The file's contain with trailing NF:s removed
+    '''
+    with open(pathname, 'r') as file:
+        return file.read().rstrip('\n')
+
+def unique(items):
+    '''
+    Keep only one copy of duplicate items
+    
+    @param   items:itr<¿V?>  The items
+    @return  :list<¿V?>      The items deduplicated
+    '''
+    rc, last = [], None
+    for item in items:
+        if item == last:
+            continue
+        rc.append(item)
+        last = item
+    return rc
+
+def strdur(seconds):
+    '''
+    Convert an amount of seconds to a human readable duration representation
+    
+    @param   seconds:float|str  The number of seconds of the duration
+    @return  :str               The duration as a string
+    '''
+    (seconds, s) = divmod(float(seconds), 60)
+    (seconds, m) = divmod(seconds, 60)
+    (d, h) = divmod(seconds, 24)
+    return '%id%02i:%02i:%05.2f' % (d, h, m, s)
+
+
+def infofunc():
+    '''
+    Print system information to stdout
+    '''
+    S = lambda cmd : spawn_pipe(*(cmd.split()))
+    uname = os.uname()
+    printtag('User', env('USER'))
+    printtag('Home', home)
+    printtag('Hostname', uname.nodename)
+    printtag('Distribution', distro)
+    osname = (S('uname --operating-system'), uname.sysname, uname.release)
+    printtag('Operating system', '%s with %s %s kernel' % osname)
+    printtag('Kernel version', uname.version)
+    printtag('Processor architecture', uname.machine)
+    if os.path.exists('/proc'):
+        try:
+            cpuinfo = cat('/proc/cpuinfo').split('\n')
+            cpu = [line for line in cpuinfo if line.startswith('model name')]
+            for cpu in [line.split(':')[1] for line in unique(sorted(cpu))]:
+                printtag('Processor model', cpu.strip())
+            cpu = sum([float(line.split(':')[1].strip()) for line in cpuinfo if line.startswith('cpu MHz')])
+            printtag('Current CPU speed', '%.0f MHz' % cpu)
+        except:
+            pass
+        try_(lambda : printtag('Load average', cat('/proc/loadavg')))
+        try_(lambda : printtag('Uptime', '%s %s' % tuple([strdur(c) for c in cat('/proc/uptime').split()])))
+        try:
+            mem = cat('/proc/meminfo').split('\n')
+            mem = dict([[cell.strip() for cell in line.split(':')[:2]] for line in mem])
+            try_(lambda : printtag('Total memory', '%s + %s swap' % (mem['MemTotal'], mem['SwapTotal'])))
+            try_(lambda : printtag('Hardware corrupted memory', mem['HardwareCorrupted']))
+            try_(lambda : printtag('Kernel memory', '%s stack + %s slab' % (mem['KernelStack'], mem['Slab'])))
+            try_(lambda : printtag('Shared memory', mem['Shmem']))
+            try_(lambda : printtag('Locked memory', '%s, %s unevictable' % (mem['Mlocked'], mem['Unevictable'])))
+            try_(lambda : printtag('Memory buffers', mem['Buffers']))
+            try_(lambda : printtag('Cached memory', '%s + %s swap' % (mem['Cached'], mem['SwapCached'])))
+        except:
+            pass
+        try:
+            route = [line.split('\t')[2] for line in cat('/proc/net/route').split('\n')]
+            route = [line for line in route if ('Gateway' not in line) and ('00000000' not in line)]
+            for g in route:
+                g = '%i.%i.%i.%i' % (int(g[6:8], 16), int(g[4:6], 16), int(g[2:4], 16), int(g[0:2], 16))
+                printtag('Default gateway', g)
+        except:
+            pass
+    printtag('Shell', env('SHELL'))
+    printtag('Shell version', spawn_pipe(env('SHELL'), '--version').split('\n')[0])
+    printtag('Teletypewriter', try_(lambda : os.ttyname(2), lambda : os.ttyname(0)))
+    printtag('Terminal', '%s %s' % (env('TERM'), env('COLORTERM')))
+    printtag('X display', env('DISPLAY'))
+    printtag('Wayland display', env('WAYLAND_DISPLAY'))
+    printtag('Window manager', env('DESKTOP_SESSION'))
+    printtag('Editor', env('EDITOR'))
+    printtag('Locale', env('LANG'))
+
+
+def invoke_read(function):
+    '''
+    Read the output to stdout make by a function
+    
+    @param   function:()→void  The function to invoke and read read stdout output from
+    @return  :str              The function's output to stdout
+    '''
+    # Create pipe for intraprocess use.
+    (r_end, w_end) = os.pipe()
+    
+    # Backup stdout.
+    stdout = os.dup(1)
+    os.close(1)
+    
+    # Replace stdout with the write end of the pipe.
+    os.dup2(w_end, 1)
+    os.close(w_end)
+    
+    # Invoke the functions, its stdout will be
+    # the write end of the pipe.
+    function()
+    # Flush output.
+    sys.stdout.buffer.flush()
+    
+    # Close the pipe.
+    os.close(1)
+    
+    # Read from the pipe.
+    rc = None
+    with os.fdopen(r_end, 'rb') as file:
+        rc = file.read()
+    
+    # Restore stdout.
+    os.dup2(stdout, 1)
+    os.close(stdout)
+    
+    # Decode output.
+    rc = rc.decode('utf-8', 'replace')
+    # Remove at most on trailing NF.
+    return rc[:-1] if rc.endswith('\n') else rc
 
 
 ## Get distribution name by way of reading /etc/os-release or /etc/lsb-release
@@ -281,18 +393,21 @@ spawn('ponysay', '-o', *pony)
 ## Reset palette after ponysay.
 if not env('PONYSAY_KMS_PALETTE', '') == '':
     # We have a palette.
-    print(env('PONYSAY_KMS_PALETTE'), end = '')
-else:
+    print(env('PONYSAY_KMS_PALETTE'), end = '', flush = False)
+elif not env('PONYSAY_KMS_PALETTE_CMD', '') == '':
     # We have a palette printing command,
     # we are wrapping it with printing so
     # that it does not print an empty line.
-    print(spawn_pipe('sh', '-c', 'exec %s' % env('PONYSAY_KMS_PALETTE_CMD')), end = '')
+    print(spawn_pipe('sh', '-c', env('PONYSAY_KMS_PALETTE_CMD')), end = '', flush = False)
 
 ## Jump to where the text should begin.
 print('\033[%i;1H' % (top + 1), end = '', flush = True)
 
-
-info="$(infofunc)" ## FIXME not ported
+## Get system information.
+info = invoke_read(infofunc)
+## Prepend whitespace to the left side of the text.
+whitespace = '' if left == 0 else ('\033[%iC' % left)
+info = whitespace + info.replace('\n', '\n' + whitespace)
 
 ## Print the information.
 print(info, end = '\n', flush = True)
